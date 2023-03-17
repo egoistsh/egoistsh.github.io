9cf916085a082c20c466c35e8b5f0df0285fa514
billy <g_billy@163.com>
2023-03-17 22:37:42 +0800

    fix: fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
-
-# 确保脚本抛出遇到的错误
 set -e
 
-push_addr=`git@github.com:egoistsh/egoistsh.github.io.git` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`deploy`
-dist_path=kaze-vuepress-doc/docs/.vuepress/dist # 打包生成的文件夹路径
+push_addr='git@github.com:egoistsh/egoistsh.github.io.git' # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+commit_info='deploy'
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 push_branch=pages # 推送的分支
 
 # 生成静态文件
 # npm run build
+cd kaze-vuepress-doc/
+export NODE_OPTIONS=--openssl-legacy-provider
 yarn build
 
 # 进入生成的文件夹
